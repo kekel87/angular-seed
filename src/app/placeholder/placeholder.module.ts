45ee83d4596298352dcf00a1d33070b8a78fa24b
Michael Parry <michael.parry.87@gmail.com>
2022-07-17 15:50:24 +0200

    feat(placeholder): change layout and add exemples
    - add responsive mixings

--- src/app/placeholder/placeholder.module.ts
+++ src/app/placeholder/placeholder.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
 
 import { PlaceholderRoutingModule } from './placeholder-routing.module';
 import { PlaceholderComponent } from './placeholder.component';
@@ -19,18 +26,26 @@ import { PlaceholderComponent } from './placeholder.component';
   declarations: [PlaceholderComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     PlaceholderRoutingModule,
-    MatIconModule,
     MatBadgeModule,
     MatButtonModule,
-    MatDividerModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
     MatChipsModule,
+    MatDatepickerModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
     MatSelectModule,
+    MatStepperModule,
+    MatTableModule,
   ],
 })
 export class PlaceholderModule {}
